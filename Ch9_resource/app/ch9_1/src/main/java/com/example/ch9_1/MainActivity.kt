2b07c71ec033a1928b5cc20628933c6b63fa57fe
Hajin74 <yoonmij9974@naver.com>
2022-07-11 23:56:08 +0900

    size, color resource

--- Ch9_resource/app/ch9_1/src/main/java/com/example/ch9_1/MainActivity.kt
+++ Ch9_resource/app/ch9_1/src/main/java/com/example/ch9_1/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.ch9_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.res.ResourcesCompat
 import com.example.ch9_1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.tv2.text = getString(R.string.txt_data2)
+        binding.tv2.setTextColor(ResourcesCompat.getColor(resources, R.color.txt_color, null))
+        binding.tv2.setBackgroundColor(ResourcesCompat.getColor(resources, R.color.txt_bg_color, null))
+        binding.tv2.textSize = resources.getDimension(R.dimen.txt_size)
     }
 }
