dbc9a3f209d347684f8224d4098f7da7bf20038c
Hajin74 <yoonmij9974@naver.com>
2022-07-11 23:42:03 +0900

    string resource

--- Ch9_resource/app/ch9_1/src/main/java/com/example/ch9_1/MainActivity.kt
+++ Ch9_resource/app/ch9_1/src/main/java/com/example/ch9_1/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.ch9_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ch9_1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tv2.text = getString(R.string.txt_data2)
     }
 }
