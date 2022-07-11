07a0552a7608a0f6f861a88284b47e1b601bb444
Hajin74 <yoonmij9974@naver.com>
2022-07-11 20:52:14 +0900

    event

---
+++ Ch8_event/ch8_1/src/main/java/com/example/ch8_1/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.ch8_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                Log.d("event", "Touch down")
+            }
+            MotionEvent.ACTION_UP -> {
+                Log.d("event", "Touch up")
+            }
+        }
+        return super.onTouchEvent(event)
+    }
+}
