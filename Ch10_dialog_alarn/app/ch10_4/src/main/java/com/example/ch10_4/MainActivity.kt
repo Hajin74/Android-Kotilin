8ab3b2bf1adad664cbbce9b56a1902e30bfee367
Hajin74 <yoonmij9974@naver.com>
2022-07-13 00:15:34 +0900

    vibrator

--- Ch10_dialog_alarn/app/ch10_4/src/main/java/com/example/ch10_4/MainActivity.kt
+++ Ch10_dialog_alarn/app/ch10_4/src/main/java/com/example/ch10_4/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.example.ch10_4
 
+import android.content.Context
 import android.media.RingtoneManager
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Vibrator
+import android.os.VibratorManager
+import androidx.core.content.getSystemService
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val notification: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-        val ringtone = RingtoneManager.getRingtone(applicationContext, notification)
-        ringtone.play()
+        val vibrator = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val vibratorManager = this.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+            vibratorManager.defaultVibrator;
+        } else {
+            getSystemService(VIBRATOR_SERVICE) as Vibrator
+        }
     }
 }
