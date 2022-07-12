4b85f3ebd1ccdfc9a1a159e63b45e488c7728330
Hajin74 <yoonmij9974@naver.com>
2022-07-13 00:11:38 +0900

    sound alarm

---
+++ Ch10_dialog_alarn/app/ch10_4/src/main/java/com/example/ch10_4/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.ch10_4
+
+import android.media.RingtoneManager
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val notification: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val ringtone = RingtoneManager.getRingtone(applicationContext, notification)
+        ringtone.play()
+    }
+}
