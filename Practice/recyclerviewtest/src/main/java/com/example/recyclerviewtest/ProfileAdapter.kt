896ec862464a067cac5780c210bf38d2aa473a67
Hajin74 <yoonmij9974@naver.com>
2022-07-14 13:39:24 +0900

    recycler view CLICK

--- Practice/recyclerviewtest/src/main/java/com/example/recyclerviewtest/ProfileAdapter.kt
+++ Practice/recyclerviewtest/src/main/java/com/example/recyclerviewtest/ProfileAdapter.kt
@@ -5,13 +5,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class ProfileAdapter(val profileList: ArrayList<Profiles>) : RecyclerView.Adapter<ProfileAdapter.CustomViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
-        return CustomViewHolder(view)
+        return CustomViewHolder(view).apply {
+            itemView.setOnClickListener {
+                val curPos: Int = adapterPosition
+                val profile: Profiles = profileList.get(curPos)
+                Toast.makeText(parent.context, "이름: ${profile.name} 나이: ${profile.age} 직업: ${profile.job}", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun getItemCount(): Int {
