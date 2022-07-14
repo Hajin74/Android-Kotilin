62c5c0a02894e3ae0bf3a2e09c8af4a8aa8d11d6
Hajin74 <yoonmij9974@naver.com>
2022-07-14 21:43:08 +0900

    Intent Test with kotlin

---
+++ Practice/app/intenttest/src/main/java/com/example/intenttest/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.intenttest
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.intenttest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnA.setOnClickListener {
+            val intent = Intent(this, SubActivity::class.java) // 다음 화면으로 이동하기 위한 인텐트 객체 생성
+            intent.putExtra("msg", binding.tvSendMsg.text.toString())
+            startActivity(intent) // intent 에 저장되어있는 액티비티 쪽으로 이동한다.
+            finish() // 자기 자신 액티비티를 파괴
+        }
+    }
+}
---
+++ Practice/app/intenttest/src/main/java/com/example/intenttest/SubActivity.kt
@@ -0,0 +1,18 @@
+package com.example.intenttest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.intenttest.databinding.ActivityMainBinding
+import com.example.intenttest.databinding.ActivitySubBinding
+
+class SubActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivitySubBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if(intent.hasExtra("msg")) {
+            binding.tvGetMsg.text = intent.getStringExtra("msg")
+        }
+    }
+}
